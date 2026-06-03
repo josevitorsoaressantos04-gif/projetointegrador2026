4598f820b1619aac81a961db626e97a24794d642
jose vitor <josevitorsoaressantos04@gmail.com>
2026-06-03 20:44:35 -0300

    tentei alterar o nome do objeto instanciao pela classe cliente porem nao deu certo

--- DAO/ClienteDAO.java
+++ DAO/ClienteDAO.java
@@ -2,7 +2,6 @@ package sistema.DAO;
 
 import sistema.conexao.ConexaoBanco;
 import sistema.model.Cliente;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -10,7 +9,6 @@ import java.sql.SQLException;
 public class ClienteDAO {
 
     public void cadastrarCliente(Cliente cliente) {
-
         String sql = """
                 INSERT INTO cliente (
                     nome,
