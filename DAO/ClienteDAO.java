acacf0cffd2c5d40da9079fbb21d179552b58eb4
jose vitor <josevitorsoaressantos04@gmail.com>
2026-06-03 21:11:47 -0300

    Definindo o develop

--- DAO/ClienteDAO.java
+++ DAO/ClienteDAO.java
@@ -6,6 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 //Main
+
+//develop
 public class ClienteDAO {
 
     public void cadastrarCliente(Cliente cliente) {
