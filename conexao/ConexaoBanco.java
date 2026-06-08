a5694e40e57d2cfc8900f862da18e5c77e4524e1
jose vitor <josevitorsoaressantos04@gmail.com>
2026-06-08 19:51:37 -0300

    Inserindo os metodos que impactam no banco de dados.

--- conexao/ConexaoBanco.java
+++ conexao/ConexaoBanco.java
@@ -3,7 +3,7 @@ package sistema.conexao;
 import java.sql.*;
 
 public class ConexaoBanco {
-    private static final String url = "jdbc:mysql://localhost:3306/erp_sistema";
+    private static final String url = "jdbc:mysql://localhost:3306/senac";
     private static final String usuario = "root";
     private static final String senha = "admin#123";
 
